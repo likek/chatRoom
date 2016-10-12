40d13a2504bff3fc4c666776601c019f359a3650
likeke <likek@outlook.com>
2016-10-12 21:55:17 +0800

    待完成

--- start.js
+++ start.js
@@ -8,14 +8,16 @@ var io=require('socket.io')(http);
 
 app.use('/node_modules',express.static('node_modules'));
 
+/*****主页******/
 app.get('/',function (req, res) {
+    response.writeHead(200, { 'Content-Type': 'text/html'});
     res.sendFile(__dirname+'/index.html');
 });
 
+/******接受客户端消息并处理*****/
 io.on('connection',function (socket) {
     console.log('一个用户连接成功');
     socket.on('tiwen',function (mes) {
-        console.log('得到了:'+mes);
         //广播
         io.emit('tiwen',mes);
     });
